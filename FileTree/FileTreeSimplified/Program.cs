14824f701aa35375ecf97aa03ed798b5a27f7497
Arthur Khrabrov <khrabrovart@gmail.com>
2018-12-10 20:11:30 +0300

    Add simplified version

---
+++ FileTree/FileTreeSimplified/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace FileTreeSimplified
+{
+    class Program
+    {
+        const string DirectoryNodeSign = "■─";
+        const string FileNodeSign = "└─";
+        const int IndentSize = 2;
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.Write("Enter directory path: ");
+            var path = Console.ReadLine();
+            PrintDirectory(path);
+
+            Console.ReadKey();
+        }
+
+        static void PrintDirectory(string path, int depth = 1)
+        {
+            if (depth == 1)
+            {
+                ColorPrint(path, ConsoleColor.Yellow);
+            }
+
+            if (!Directory.Exists(path))
+            {
+                return;
+            }
+
+            var files = Directory.GetFiles(path);
+
+            foreach (var file in files)
+            {
+                var fileName = Path.GetFileName(file);
+                ColorPrint($"{GetIndent(depth)}{FileNodeSign}{fileName}", ConsoleColor.White);
+            }
+
+            var directories = Directory.GetDirectories(path);
+
+            foreach (var directory in directories)
+            {
+                var directoryName = Path.GetFileName(directory);
+                ColorPrint($"{GetIndent(depth)}{DirectoryNodeSign}{directoryName}", ConsoleColor.Yellow);
+
+                PrintDirectory(directory, depth + 1);
+            }
+        }
+
+        static string GetIndent(int depth)
+        {
+            return new string(' ', depth * IndentSize);
+        }
+
+        static void ColorPrint(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
